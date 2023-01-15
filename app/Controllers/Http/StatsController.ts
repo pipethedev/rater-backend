1f097650d759b27882873fa72e66f4212408f7cb
Muritala David <davmuri1414@gmail.com>
2023-01-15 20:05:32 +0100

    worker rating

--- app/Controllers/Http/StatsController.ts
+++ app/Controllers/Http/StatsController.ts
@@ -1,7 +1,8 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Database from '@ioc:Adonis/Lucid/Database'
-import { Roles } from 'App/Enum'
+import { RatingLevel, Roles } from 'App/Enum'
 import { SuccessResponse, convertToNaria } from 'App/Helpers'
+import Rating from 'App/Models/Rating'
 import Song from 'App/Models/Song'
 import User from 'App/Models/User'
 
@@ -19,7 +20,11 @@ export default class StatsController {
                 revenue = await Database.rawQuery('SELECT SUM(amount) as total FROM transactions')
                 return response.ok( SuccessResponse("Dashbord stats", { songs, users, revenue: convertToNaria(revenue[0][0].total)}))
             case Roles.MANAGER:
-                break;
+                songs = await Rating.query().where('worker_id', id)
+                const good = await Rating.query().where('worker_id', id).where('rating', RatingLevel.Good)
+                const fair = await Rating.query().where('worker_id', id).where('rating', RatingLevel.Fair)
+                const bad = await Rating.query().where('worker_id', id).where('rating', RatingLevel.Bad)
+                return response.ok( SuccessResponse("Dashbord stats", { songs: songs.length, good: good.length, fair: fair.length, bad: bad.length }))
             case Roles.USER:
                 songs = (await Song.query().where('user_id', id)).length
                 ratings = await Database.rawQuery('SELECT AVG(rating) as avg_rating FROM ratings WHERE user_id = ?', [id])
